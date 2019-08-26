440e14c98774a4ace56af3074ac775e9dfeeb60b
Michael Lange <dingoeatingfuzz@gmail.com>
2019-08-26 01:24:24 -0700

    Finish the api function

--- api.py
+++ api.py
@@ -1,7 +1,12 @@
 import gspread
 import json
 from oauth2client.service_account import ServiceAccountCredentials
+import boto3
+import os
 
+AWS_PROFILE = os.environ.get("AWS_PROFILE")
+aws_session = boto3.session.Session(profile_name=AWS_PROFILE)
+aws = aws_session.client("ssm")
 
 # Lower case key names and trim white space
 def normalize(record):
@@ -10,9 +15,12 @@ def normalize(record):
 
 def get_roster():
     scope = ["https://www.googleapis.com/auth/drive.readonly"]
-    creds = ServiceAccountCredentials.from_json_keyfile_name(
-        "gcp_credentials.json", scope
+    ssm_response = aws.get_parameter(
+        Name="/production/team-roster/gcp-credentials", WithDecryption=True
     )
+
+    creds_json = json.loads(ssm_response["Parameter"]["Value"])
+    creds = ServiceAccountCredentials.from_json_keyfile_dict(creds_json, scopes=scope)
     client = gspread.authorize(creds)
 
     # WARNING: This finds a sheet by name, if the sheet name changes, this will go boom
@@ -20,12 +28,14 @@ def get_roster():
 
     rows = sheet.get_all_records()
     rows = [normalize(x) for x in rows]
+    return rows
 
-    res = {"data": rows}
-    return res
+
+def lambda_handler(event, context):
+    return {"statusCode": 200, "data": get_roster()}
 
 
 # When running locally
 if __name__ == "__main__":
-    print(json.dumps(get_roster()))
+    print(json.dumps(lambda_handler()))
 
